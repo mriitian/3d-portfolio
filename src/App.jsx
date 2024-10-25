dc556c022c695ad7d4e583b5c3c6ec35c14f3bce
Vedant Parkhe <vedantparkhe2409@gmail.com>
2024-10-25 14:20:00 +0530

    stars fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { BrowserRouter } from "react-router-dom"
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 const App = () => {
-
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
-      <StarsCanvas />
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
@@ -16,12 +24,13 @@ const App = () => {
         <Tech />
         <Works />
         <div className="relative z-0 ">
+          <StarsCanvas />
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
